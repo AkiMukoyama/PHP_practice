fdc6a9b7da878716eb1938c857021c85d7bdd450
AkiMukoyama <aki.fujimatsu@gmail.com>
2020-08-22 04:34:01 +0000

    first commit

---
+++ practice02.php
@@ -0,0 +1,8 @@
+<?php
+$a = 3;
+$b = 7;
+$sum = $a + $b;
+echo $sum;
+
+
+
---
+++ practice_04.php
@@ -0,0 +1,29 @@
+<?php
+//1
+function double($num){
+    echo $num*2;
+}
+
+echo double(5);
+echo "\n";
+
+//2
+function sum($num1,$num2){
+    echo $num1+$num2;
+}
+sum(1,3);
+echo "\n";
+
+//3
+
+$arr = array(1,3,5,7,9);
+$result = array_product( $arr ) ;
+
+	echo "\n";
+	echo $result;
+
+
+
+?>
+
+
